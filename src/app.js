0862bc605f26490536c51feb6cb1a64e05fcef1e
German <salomeescalante9@gmail.com>
2022-11-02 21:28:20 -0500

    completando recipes.controller

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 //? Dependencies
 const express = require('express');
+const cors = require('cors')
 const db = require('./utils/database')
 
 //? Files
@@ -8,7 +9,7 @@ const {port} = require('./config');
 const userRouter = require('./users/users.router')
 const authRouter = require('./auth/auth.router')
 const categoryRouter = require('./categories/categories.router')
-
+const recipeRouter = require('./recipes/recipes.router')
 
 const initModels = require('./models/initModels')
 
@@ -16,6 +17,8 @@ const initModels = require('./models/initModels')
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 db.authenticate()
     .then(() => {
@@ -46,6 +49,7 @@ app.get('/',(req, res) => {
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/categories', categoryRouter)
+app.use('/api/v1/recipes', recipeRouter)
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
--- src/recipes/recipes.controllers.js
+++ src/recipes/recipes.controllers.js
@@ -1,25 +1,77 @@
+const uuid = require('uuid')
 const Recipes = require('../models/recipes.models')
+const Users = require('../models/users.models')
+const Categories = require('../models/categories.models')
+const Instructions = require('../models/instructions.models')
+const RecipeIngredients = require('../models/recipes_ingredients.models')
+const Ingredients = require('../models/ingredients.models')
+const Types = require('../models/types.models')
+
 
 const getAllRecipes = async () => {
-    const data = await Recipes.findAll()
+    const data = await Recipes.findAll({
+        attributes: {
+            exclude: ['userId', 'categoryId', 'createdAt', 'updatedAt']
+        },
+        include: [
+            {
+                model: Categories
+            },
+            {
+                model: Users,
+                attributes: ['id', 'firstName', 'lastName']
+            },
+            {
+                model: Instructions,
+                attributes: ['step', 'description']
+            },
+            {
+                model: RecipeIngredients,
+                include: {
+                    model: Ingredients,
+                    include: {
+                        model: Types
+                    }
+                }
+            }
+        ]
+
+    })
     return data
 }
 
-const getRecipesById = async (id) => {
-    const data = await Recipes.finOne({
+const getRecipeById = async (id) => {
+    const data = await Recipes.findOne({
         where: {
             id
         }
-
-    })
-    return data    
-}
-
-const createRecipe = async (name) => {
-    const data = await Categories.create({
-        name
     })
     return data
+}
+
+const createRecipe = async (data) => {
+    const response = await Recipes.create({
+        id: uuid.v4(),
+        title: data.title,
+        description: data.description,
+        urlImg: data.urlImg,
+        time: data.time,
+        portions: data.portions,
+        userId: data.userId,
+        categoryId: data.categoryId,
+        origin: data.origin,
+        likes: data.likes
+    })
+    return response
+}
+
+const updateRecipe = async (id, data) => {
+    const response = await Recipes.update(data, {
+        where: {
+            id
+        }
+    })
+    return response
 }
 
 const deleteRecipe = async (id) => {
@@ -33,7 +85,8 @@ const deleteRecipe = async (id) => {
 
 module.exports = {
     getAllRecipes,
-    getRecipesById,
+    getRecipeById,
     createRecipe,
+    updateRecipe,
     deleteRecipe
 }
